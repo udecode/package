489510f56046744da5e7df0db1e5ace99ac8478c
zbeyens <zbeyens@udecode.dev>
2025-10-21 12:58:23 +0200

    biome

---
+++ CLAUDE.md
@@ -0,0 +1,1 @@
+@.cursor/rules/ultracite.mdc
--- prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-export default {
-  endOfLine: 'lf',
-  plugins: ['prettier-plugin-packagejson', 'prettier-plugin-jsdoc'],
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'es5',
-};
